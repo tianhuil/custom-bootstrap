33fdc19c797f14346b6f53f49a6d2d776d3f53e9
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2019-02-09 20:47:15 -0800

    Baseline files

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,42 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+// sourcemaps usage from
+// https://blog.yipl.com.np/using-gulp-browser-sync-and-source-maps-ef14e0903982
+var sourcemaps  = require('gulp-sourcemaps');
 
 sass.compiler = require('node-sass');
 
 gulp.task('sass', function () {
-  return gulp.src('./scss/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));
+  return gulp.src('./src/scss/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(
+      sass({
+        'onError': browserSync.notify
+      })
+    )
+    .pipe(sourcemaps.write('./src/out/maps'))
+    .pipe(gulp.dest('./src/out/css'))
+    .pipe(browserSync.stream());
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./scss/*.scss', gulp.series('sass'));
+gulp.task('js', function() {
+  return gulp.src(["src/js/*.js"])
+    .pipe(gulp.dest("src/out/js"))
+    .pipe(browserSync.stream());
 });
+
+gulp.task('serve', ['sass', 'js'], function() {
+
+    browserSync.init({
+        server: "./src"
+    });
+
+    gulp.watch(['src/scss/*.scss'], ['sass']);
+    gulp.watch(['src/js/*.js'], ['js']);
+    gulp.watch("src/*.html").on('change', browserSync.reload);
+});
+
+gulp.task('default', ['js','serve']);
